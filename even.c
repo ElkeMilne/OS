ec15199f7b675ca5f3c0cd07c5da597894ba0eb2
ElkeMilne <elke.milne@student.adelaide.edu.au>
2025-08-24 14:39:58 +0930

    even.c

---
+++ even.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+
+void handle_input(int n){
+    if(n == SIGHUP){
+        printf("Ouch!\n");
+    }
+    else if(n == SIGINT){
+        printf("Yeah!\n"){
+
+        }
+    }
+}
+//commant line arguments passed into main
+int main(int argc, char *argv[]){
+
+//need to convert string to int
+int convertN = atoi(argv[1]);
+
+//override default behaviour
+signal(SIGHUP, handle_input);
+signal(SIGINT, handle_input);
+
+for (int i = 0; i < convertN; i++){
+    if(i % 2 == 0){
+        printf("%d\n", i);
+        fflush(stdout);
+        sleep(5);
+    }
+}
+}
