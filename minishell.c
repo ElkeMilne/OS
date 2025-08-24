d2c73544b52dcdca786b2f35ca74e459d555b581
ElkeMilne <elke.milne@student.adelaide.edu.au>
2025-08-24 15:10:33 +0930

    minishell

--- minishell.c
+++ minishell.c
@@ -1,11 +1,11 @@
 /*********************************************************************
-   Program  : miniShell                   Version    : 1.3
+   Program  : miniShell                   Version    : 1.4
  --------------------------------------------------------------------
-   skeleton code for linix/unix/minix command line interpreter
+   Modified skeleton code for Linux/Unix command line interpreter
+   Adds backgroundFlag jobs, cd command, and proper error handling
  --------------------------------------------------------------------
-   File			: minishell.c
-   Compiler/System	: gcc/linux
-
+   File       : minishell.c
+   Compiler/System : gcc/linux
 ********************************************************************/
 
 #include <sys/types.h>
@@ -15,16 +15,28 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <errno.h>
 
-#define NV 20			/* max number of command tokens */
-#define NL 100			/* input buffer size */
-char            line[NL];	/* command input buffer */
+#define NV 20           /* max number of command tokens */
+#define NL 100          /* input buffer size */
+#define MAX_JOBS 100    /* maximum number of backgroundFlag jobs */
 
+char line[NL];          /* command input buffer */
+
+/* structure to store backgroundFlag job info */
+typedef struct {
+    int job_id;
+    pid_t pid;
+    char command[NL];
+} Job;
+
+Job jobs[MAX_JOBS];             /* active backgroundFlag jobs */
+int job_count = 0;              /* number of active jobs */
+int job_id_counter = 1;         /* auto-increment job ID */
 
 /*
-	shell prompt
- */
-
+    shell prompt
+*/
 void prompt(void)
 {
   // ## REMOVE THIS 'fprintf' STATEMENT BEFORE SUBMISSION
@@ -32,58 +44,136 @@ void prompt(void)
   fflush(stdout);
 }
 
+/*
+    signal handler for SIGCHLD
+    reports when backgroundFlag process finishes immediately
+*/
+void sighandler(int sig)
+{
+    int status;
+    pid_t pid;
+
+    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
+        for (int i = 0; i < job_count; i++) {
+            if (jobs[i].pid == pid) {
+
+                /* print job finished immediately */
+                if (WIFEXITED(status)) {
+                    printf("\n[%d]+ Done %s (exit=%d)\n", jobs[i].job_id, jobs[i].command, WEXITSTATUS(status));
+                } else if (WIFSIGNALED(status)) {
+                    printf("\n[%d]+ Terminated %s (signal=%d)\n", jobs[i].job_id, jobs[i].command, WTERMSIG(status));
+                }
+                fflush(stdout);
+
+                /* remove job from active job list */
+                for (int j = i; j < job_count - 1; j++) {
+                    jobs[j] = jobs[j + 1];
+                }
+                job_count--;
+                break;
+            }
+        }
+    }
+}
 
 /* argk - number of arguments */
 /* argv - argument vector from command line */
 /* envp - environment pointer */
 int main(int argk, char *argv[], char *envp[])
 {
-   int             frkRtnVal;	    /* value returned by fork sys call */
-  char           *v[NV];	        /* array of pointers to command line tokens */
-  char           *sep = " \t\n";  /* command line token separators    */
-  int             i;		          /* parse index */
+    int frkRtnVal;          /* value returned by fork sys call */
+    char *v[NV];            /* array of pointers to command line tokens */
+    char *sep = " \t\n";    /* command line token separators */
+    int i;                  /* parse index */
+    int backgroundFlag;     /* backgroundFlag flag */
 
-    /* prompt for and process one command line at a time  */
+    /* install SIGCHLD handler to track finished backgroundFlag jobs */
+    signal(SIGCHLD, sighandler);
 
-  while (1) {			/* do Forever */
-    prompt();
-    fgets(line, NL, stdin);
-    fflush(stdin);
+    /* prompt for and process one command line at a time */
+    while (1) {              /* do forever */
+        prompt();
 
-    // This if() required for gradescope
-    if (feof(stdin)) {		/* non-zero on EOF  */
-      exit(0);
+        if (fgets(line, NL, stdin) == NULL) {
+            // This if() required for gradescope
+            if (feof(stdin)) {
+                exit(0);
+            }
+            perror("fgets");
+            continue;
+        }
+
+        /* ignore blank lines or comments */
+        if (line[0] == '#' || line[0] == '\n' || line[0] == '\000') {
+            continue;          /* back to prompt */
+        }
+
+        /* copy line for job tracking */
+        char line_copy[NL];
+        strcpy(line_copy, line);
+
+        /* tokenize input line */
+        v[0] = strtok(line, sep);
+        for (i = 1; i < NV; i++) {
+            v[i] = strtok(NULL, sep);
+            if (v[i] == NULL)
+                break;
+        }
+        v[i] = NULL;          /* ensure NULL-terminated */
+        if (v[0] == NULL) continue;
+
+        /* check for backgroundFlag '&' */
+        backgroundFlag = 0;
+        if (i > 1 && strcmp(v[i - 1], "&") == 0) {
+            backgroundFlag = 1;
+            v[i - 1] = NULL;
+            line_copy[strlen(line_copy) - 2] = '\0'; /* remove '&' from command copy */
+        }
+
+        /* handle built-in cd command */
+        if (strcmp(v[0], "cd") == 0) {
+        int ret;
+        if (v[1] == NULL) {
+            ret = chdir(getenv("HOME"));   /* default HOME */
+        } else {
+            ret = chdir(v[1]);
+        }
+        if (ret != 0) {  /* check for errors */
+            perror("cd failed");
+        }
+        continue;  /* back to prompt */
     }
-    if (line[0] == '#' || line[0] == '\n' || line[0] == '\000'){
-      continue;			/* to prompt */
-    }
 
-    v[0] = strtok(line, sep);
-    for (i = 1; i < NV; i++) {
-      v[i] = strtok(NULL, sep);
-      if (v[i] == NULL){
-	      break;
-      }
-    }
-    /* assert i is number of tokens + 1 */
-
-    /* fork a child process to exec the command in v[0] */
-    switch (frkRtnVal = fork()) {
-      case -1:			/* fork returns error to parent process */
-      {
-	      break;
-      }
-      case 0:			/* code executed only by child process */
-      {
-	      execvp(v[0], v);
-      }
-      default:			/* code executed only by parent process */
-      {
-      	wait(0);
-        // REMOVE PRINTF STATEMENT BEFORE SUBMISSION
-        printf("%s done \n", v[0]);
-    	  break;
-      }
-    }				/* switch */
-  }				/* while */
-}				/* main */
+        /* fork a child process to exec the command in v[0] */
+        switch (frkRtnVal = fork()) {
+            case -1: {          /* fork error */
+                perror("fork failed");
+                break;
+            }
+            case 0: {           /* child process */
+                execvp(v[0], v);
+                perror("exec failed");  /* exec failed */
+                exit(1);
+            }
+            default: {          /* parent process */
+                if (backgroundFlag == 0) {
+                    waitpid(frkRtnVal, NULL, 0);   /* foreground job waits */
+                } else {
+                    /* store backgroundFlag job */
+                    if (job_count < MAX_JOBS) {
+                        jobs[job_count].job_id = job_id_counter++;
+                        jobs[job_count].pid = frkRtnVal;
+                        strcpy(jobs[job_count].command, line_copy);
+                        job_count++;
+                        fprintf(stdout, "[%d] %d\n", jobs[job_count - 1].job_id, frkRtnVal);
+                        fflush(stdout);
+                    } else {
+                        fprintf(stdout, "Too many backgroundFlag jobs\n");
+                        fflush(stdout);
+                    }
+                }
+                break;
+            }
+        } /* switch */
+    } /* while */
+} /* main */
